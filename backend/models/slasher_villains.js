3f803bcd76a6e2e153397c2ae85e5b621ca98b1c
Anavi-K <anavikothari@outlook.com>
2024-02-29 10:05:01 +0530

    connected server and client

--- backend/models/slasher_villains.js
+++ backend/models/slasher_villains.js
@@ -7,7 +7,7 @@ const SlasherVillainsSchema = new mongoose.Schema({
     weapons: Array,
     modus_operandi: String,
     motivation_background: String,
-    kill_count: String,
+    kill_count: Number,
     weakness: String
 })
 
